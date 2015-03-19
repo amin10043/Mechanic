0e8d73c0ae5d87f50e4d1c1cd458174aa5fc18ed
Amin Hosseini <amin10043@gmail.com>
2015-03-19 15:22:44 +0330

    merging 2

---
+++ src/com/project/mechanic/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.project.mechanic;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+
+	int SPLASH_DISPLAY_TIME = 3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+
+				Intent intent = new Intent();
+				intent.setClass(SplashActivity.this, MainActivity.class);
+
+				startActivity(intent);
+				finish();
+
+				// transition from splash to main menu
+				overridePendingTransition(R.layout.splash_out,
+						R.layout.splash_in);
+
+			}
+		}, SPLASH_DISPLAY_TIME);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_splash, menu);
+		return true;
+	}
+
+}
