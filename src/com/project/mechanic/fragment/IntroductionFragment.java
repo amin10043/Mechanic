400ad7aa37a911836789d26dcaf5d30529f5808c
Shaghayegh <shaghayegh.kalalimoghadam@gmail.com>
2015-03-20 17:26:47 +0330

    ADD Introduction Fragment

---
+++ src/com/project/mechanic/fragment/IntroductionFragment.java
@@ -0,0 +1,33 @@
+package com.project.mechanic.fragment;
+
+
+
+import com.project.mechanic.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+
+public class IntroductionFragment extends Fragment {
+	
+      @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    		Bundle savedInstanceState) {
+    	  View view = inflater.inflate(R.layout.activity_introduction, null);
+    	// TODO Auto-generated method stub
+    	return view;
+    	
+    	
+    }
+	
+	
+	
+}
