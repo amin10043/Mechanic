d9a5fc79a35c188aadd9715b217ab6c3a1a4bd7c
Shaghayegh <shaghayegh.kalalimoghadam@gmail.com>
2015-03-20 17:44:34 +0330

    insert button

--- src/com/project/mechanic/fragment/IntroductionFragment.java
+++ src/com/project/mechanic/fragment/IntroductionFragment.java
@@ -21,9 +21,8 @@ public class IntroductionFragment extends Fragment {
       @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
-    	  View view = inflater.inflate(R.layout.activity_introduction, null);
-    	// TODO Auto-generated method stub
-    	return view;
+    	    View view = inflater.inflate(R.layout.activity_introduction, null);
+    	   return view;
     	
     	
     }
