7e4a8a1b7f6451b3a89492eb6e5ce1f2dfa1ccc7
arabian <m.arabian55@yahoo.com>
2015-03-31 19:26:58 +0430

    فرگمنت دیگه

---
+++ src/com/project/mechanic/fragment/TubeFragment.java
@@ -0,0 +1,47 @@
+package com.project.mechanic.fragment;
+
+
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.project.mechanic.R;
+import com.project.mechanic.adapter.TubeListAdapter;
+import com.project.mechanic.entity.ListItem;
+import com.project.mechanic.model.DataBaseAdapter;
+
+public class TubeFragment extends Fragment {
+
+
+	DataBaseAdapter dbAdapter;
+
+	@SuppressLint("InflateParams")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_tube, null);
+		dbAdapter = new DataBaseAdapter(getActivity());
+
+		dbAdapter.open();
+		List<ListItem> mylist = dbAdapter.getListItemsById(9);
+		dbAdapter.close();
+
+		ListView lstTube = (ListView) view.findViewById(R.id.lstVtube);
+		TubeListAdapter ListAdapter = new TubeListAdapter(getActivity(),
+				R.layout.row_tube, mylist);
+
+		lstTube.setAdapter(ListAdapter);
+
+
+		
+		return view;
+	}
+}
+
