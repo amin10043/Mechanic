fb7b566c73d854491f3ccd300d118774c6abed4f
arabian <m.arabian55@yahoo.com>
2015-04-08 13:00:20 +0430

    اضافه کردن لیست وعکس آگهی

--- src/com/project/mechanic/fragment/AnadFragment.java
+++ src/com/project/mechanic/fragment/AnadFragment.java
@@ -1,17 +1,22 @@
 package com.project.mechanic.fragment;
 
+import java.util.List;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import com.project.mechanic.MainActivity;
 import com.project.mechanic.R;
+import com.project.mechanic.adapter.AnadListAdapter;
+import com.project.mechanic.entity.ListItem;
+import com.project.mechanic.model.DataBaseAdapter;
 
 public class AnadFragment extends Fragment {
 
-	
+	DataBaseAdapter dbAdapter;
 	
 
 	@SuppressLint("InflateParams")
@@ -22,7 +27,17 @@ public class AnadFragment extends Fragment {
 
 		((MainActivity) getActivity()).setActivityTitle(R.string.anad);
 
-	
+			dbAdapter = new DataBaseAdapter(getActivity());
+
+			dbAdapter.open();
+			List<ListItem> mylist = dbAdapter.getListItemsById(0);
+			dbAdapter.close();
+
+			ListView lstAnad = (ListView) view.findViewById(R.id.listVanad);
+			AnadListAdapter ListAdapter = new AnadListAdapter(getActivity(),
+					R.layout.row_anad, mylist);
+
+			lstAnad.setAdapter(ListAdapter);
 		
 		
 		
