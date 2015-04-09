9c3e683b1b94aa6f7045e89fa5cf7edf5d68c95f
arabian <m.arabian55@yahoo.com>
2015-04-09 18:28:33 +0430

    اضافه کردن فرگمنت

--- src/com/project/mechanic/adapter/AnadListAdapter.java
+++ src/com/project/mechanic/adapter/AnadListAdapter.java
@@ -2,7 +2,7 @@ package com.project.mechanic.adapter;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -14,7 +14,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.project.mechanic.MainActivity;
 import com.project.mechanic.R;
 import com.project.mechanic.entity.ListItem;
@@ -25,6 +24,7 @@ import com.project.mechanic.fragment.ExecutertypeFragment;
 import com.project.mechanic.fragment.FroumtitleFragment;
 import com.project.mechanic.fragment.NewsFragment;
 import com.project.mechanic.fragment.ProvinceFragment;
+import com.project.mechanic.fragment.ShowAdFragment;
 import com.project.mechanic.model.DataBaseAdapter;
 
 public class AnadListAdapter extends ArrayAdapter<ListItem> {
@@ -44,6 +44,7 @@ public class AnadListAdapter extends ArrayAdapter<ListItem> {
 
 	}
 
+	@SuppressLint("ViewHolder")
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 
@@ -60,94 +61,18 @@ public class AnadListAdapter extends ArrayAdapter<ListItem> {
 		txtName.setText(tempItem.getName());
 		
 
-//		convertView.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//
-//				LinearLayout parentlayout = (LinearLayout) v;
-//				TextView txtName = (TextView) parentlayout
-//						.findViewById(R.id.txtName);
-//				String item = txtName.getText().toString();
-//
-//				adapter.open();
-//				ArrayList<ListItem> allItems = adapter.getListItemsById(0);
-//				int id = 0;
-//				for (ListItem listItem : allItems) {
-//					if (item.equals(listItem.getName())) {
-//						// check authentication and authorization
-//						id = listItem.getId();
-//					}
-//				}
-//				adapter.close();
-//
-//				if (id == 1) {
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					BerandFragment fragment = new BerandFragment();
-//					Bundle bundle = new Bundle();
-//					bundle.putString("Id", String.valueOf(id));
-//					fragment.setArguments(bundle);
-//					trans.replace(R.id.content_frame, fragment);
-//					trans.addToBackStack(null);
-//					trans.commit();
-//
-//				} else if (id == 2) {
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					trans.replace(R.id.content_frame, new ProvinceFragment());
-//					trans.addToBackStack(null);
-//					trans.commit();
-//
-//				} else if (id == 3) {
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					trans.replace(R.id.content_frame, new AdvisorTypeFragment());
-//					trans.addToBackStack(null);
-//					trans.commit();
-//
-//				} else if (id == 4) {
-//
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					trans.replace(R.id.content_frame,
-//							new ExecutertypeFragment());
-//					trans.addToBackStack(null);
-//					trans.commit();
-//
-//				} else if (id == 5) {
-//
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					NewsFragment fragment = new NewsFragment();
-//					Bundle bundle = new Bundle();
-//					bundle.putString("Id", String.valueOf(id));
-//					fragment.setArguments(bundle);
-//					trans.replace(R.id.content_frame, fragment);
-//					trans.addToBackStack(null);
-//					trans.commit();
-//
-//				} else if (id == 6) {
-//
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					AdvertisementFragment fragment = new AdvertisementFragment();
-//					Bundle bundle = new Bundle();
-//					bundle.putString("Id", String.valueOf(id));
-//					fragment.setArguments(bundle);
-//					trans.replace(R.id.content_frame, fragment);
-//					trans.addToBackStack(null);
-//					trans.commit();
-//					
-//				} else if (id == 7) {
-//					FragmentTransaction trans = ((MainActivity) context)
-//							.getSupportFragmentManager().beginTransaction();
-//					trans.replace(R.id.content_frame, new FroumtitleFragment());
-//					trans.addToBackStack(null);
-//					trans.commit();
-//				}
-//			}
-//		});
+		convertView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				FragmentTransaction trans = ((MainActivity) context)
+						.getSupportFragmentManager().beginTransaction();
+				trans.replace(R.id.content_frame, new ShowAdFragment());
+				trans.addToBackStack(null);
+				trans.commit();
+					}
+		});
 
 		return convertView;
 
