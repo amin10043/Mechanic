2bf416af6ac799790312cf3aacf3445e3b95aba6
Sisi <Sisi@Sisi-PC>
2015-04-19 15:53:18 +0430

    تغییرات صفحه اسپلش

--- src/com/project/mechanic/WelcomeScreen.java
+++ src/com/project/mechanic/WelcomeScreen.java
@@ -2,7 +2,9 @@ package com.project.mechanic;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Point;
@@ -11,53 +13,55 @@ import android.os.Handler;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.ListAdapter;
 
 import com.project.mechanic.adapter.WelcomeScreenAdapter;
 
 public class WelcomeScreen extends Activity {
-	int SPLASH_DISPLAY_TIME = 1000;
-	Handler handler;
-	Runnable runnable;
+	// int SPLASH_DISPLAY_TIME = 30000;
+	// Handler handler;
+	// Runnable runnable;
 
 	private WelcomeScreenAdapter adapter;
 	private GridView gridView;
 	private int columnWidth;
 	private int column = 3;
-	int gridePadding = 0;
-	private LinearLayout ln_row1, ln_row2;
-	private ImageButton img1, img2;
+	private int gridePadding = 0;
+	private ImageButton btnNext, btnExit, btnins1, btnint1, btngp1, btnfb1,
+			btntw1, btnlink1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome_screen);
 
-		handler = new Handler();
-		runnable = new Runnable() {
-			public void run() {
-
-				Intent intent = new Intent();
-				intent.setClass(WelcomeScreen.this, MainActivity.class);
-
-				startActivity(intent);
-				finish();
-
-				overridePendingTransition(R.layout.splash_out,
-						R.layout.splash_in);
-
-			}
-		};
-		handler.postDelayed(runnable, SPLASH_DISPLAY_TIME);
+		initialize();
+		cliclItem();
+		//
+		// handler = new Handler();
+		// runnable = new Runnable() {
+		// public void run() {
+		//
+		// Intent intent = new Intent();
+		// intent.setClass(WelcomeScreen.this, MainActivity.class);
+		//
+		// startActivity(intent);
+		// finish();
+		//
+		// overridePendingTransition(R.layout.splash_out,
+		// R.layout.splash_in);
+		//
+		// }
+		// };
+		// handler.postDelayed(runnable, SPLASH_DISPLAY_TIME);
 
 		gridView = (GridView) findViewById(R.id.grid_view);
-		int[] image = { R.drawable.tayan, R.drawable.tayan, R.drawable.tayan,
+		int[] image = { R.drawable.up2, R.drawable.on2, R.drawable.or2,
+				R.drawable.g1, R.drawable.g2, R.drawable.g3,
+				R.drawable.tayan, R.drawable.tayan, R.drawable.tayan,
 				R.drawable.tayan, R.drawable.tayan, R.drawable.tayan,
 				R.drawable.tayan, R.drawable.tayan, R.drawable.tayan,
 				R.drawable.tayan, R.drawable.tayan, R.drawable.tayan,
@@ -78,24 +82,7 @@ public class WelcomeScreen extends Activity {
 		gridView.setHorizontalSpacing((int) 0);
 		gridView.setVerticalSpacing((int) 0);
 
-		// ln_row1 = new LinearLayout(this);
-		// ln_row1.setOrientation(LinearLayout.HORIZONTAL);
-		// // LayoutParams linLayoutParam = new
-		// LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		//
-		// img1 = new ImageButton(this);
-		// img2 = new ImageButton(this);
-		//
-		// img1.setBackgroundResource(R.drawable.like);
-		// img2.setBackgroundResource(R.drawable.save);
-		//
-		// ln_row1.addView(img1);
-		// ln_row1.addView(img2);
-		//
-		// gridView.addView(ln_row1);
-
 		gridView.setAdapter(new WelcomeScreenAdapter(this, image, columnWidth));
-		resizeGridView(gridView);
 
 	}
 
@@ -118,30 +105,59 @@ public class WelcomeScreen extends Activity {
 
 	}
 
-	private void resizeGridView(final GridView gridview) {
-		ListAdapter listAdapter = gridview.getAdapter();
-		if (listAdapter == null) {
-			// pre-condition
-			return;
-		}
+	private void initialize() {
 
-		int totalHeight = gridview.getPaddingTop()
-				+ gridview.getPaddingBottom();
-		for (int i = 0; i < listAdapter.getCount(); i++) {
-			View listItem = listAdapter.getView(i, null, gridview);
+		btnNext = (ImageButton) findViewById(R.id.btnnext);
+		btnExit = (ImageButton) findViewById(R.id.btnprev);
 
-			if (listItem instanceof ViewGroup) {
-				listItem.setLayoutParams(new LayoutParams(
-						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		btnins1 = (ImageButton) findViewById(R.id.btninstegram);
+		btnint1 = (ImageButton) findViewById(R.id.btn_internet);
+		btngp1 = (ImageButton) findViewById(R.id.btngplas);
+		btnfb1 = (ImageButton) findViewById(R.id.btnfb);
+		btntw1 = (ImageButton) findViewById(R.id.btntwitter);
+		btnlink1 = (ImageButton) findViewById(R.id.btnlinking);
+
+	} // end initialize
+
+	private void cliclItem() {
+
+		btnNext.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(WelcomeScreen.this, MainActivity.class);
+
+				startActivity(intent);
+				finish();
+
+				overridePendingTransition(R.layout.splash_out,
+						R.layout.splash_in);
+
 			}
-			listItem.measure(0, 0);
-			totalHeight += listItem.getMeasuredHeight();
-		}
+		});
 
-		ViewGroup.LayoutParams params = gridview.getLayoutParams();
-		params.height = totalHeight
-				+ (gridview.getHeight() * (listAdapter.getCount() - 1));
-		gridview.setLayoutParams(params);
-	}
+		btnExit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				new AlertDialog.Builder(WelcomeScreen.this)
+						.setTitle("خروج از برنامه")
+						.setMessage("آیا از خروج اطمینان دارید؟")
+						.setNegativeButton("خیر", null)
+						.setPositiveButton("بله",
+								new DialogInterface.OnClickListener() {
+
+									public void onClick(DialogInterface arg0,
+											int arg1) {
+										finish();
+										System.exit(0);
+									}
+								}).create().show();
+
+			}
+		});
+
+	} // end cliclItem
 
 }
