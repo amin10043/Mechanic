e28938cbc34d9c15649566dfe052680545713beb
BD <BD@zahra>
2015-04-20 16:10:24 +0100

    تغییرات ظاهرلاگین

--- src/com/project/mechanic/fragment/RegisterFragment.java
+++ src/com/project/mechanic/fragment/RegisterFragment.java
@@ -6,8 +6,14 @@ import java.util.jar.Attributes.Name;
 import com.project.mechanic.R;
 import com.project.mechanic.model.DataBaseAdapter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -18,13 +24,37 @@ import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class RegisterFragment extends Fragment {
 
-	
+	protected static final Context Contaxt = null;
+	int resourceId;
+	 Context context;
+	 Fragment fragment;
+    int ticketTypeID;
+	 int ProvinceId;
+	 
+	 
+	 
+	 
+//	public RegisterFragment(Context context, int resourceId, Fragment fragment,
+//			int ticketTypeID, int ProvinceId) {
+//		
+//		// TODO Auto-generated constructor stub
+//		this.resourceId = resourceId;
+//		this.context = context;
+//		this.fragment = fragment;
+//		this.ticketTypeID = ticketTypeID;
+//		this.ProvinceId = ProvinceId;
+//	}
+
+	protected static final int RESULT_LOAD_IMAGE = 1;
 	DataBaseAdapter dbAdapter;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -35,6 +65,7 @@ public class RegisterFragment extends Fragment {
 			
 	Button   btncan    = (Button) view.findViewById(R.id.btncancle2);
 	Button   btnreg    =(Button) view.findViewById(R.id.btnreg2);
+	ImageButton   btnaddpic = (ImageButton) view.findViewById(R.id.btnaddpic);
 	final EditText editname  = (EditText)view.findViewById(R.id.editTextname);		 
     final EditText edituser  = (EditText)view.findViewById(R.id.editTextuser);		 
 	final EditText editpass  = (EditText)view.findViewById(R.id.editTextpass);
@@ -93,12 +124,54 @@ Toast.makeText(getActivity(), "اطلاعات مورد نظر ثبت شد", Toas
 		 	 
 		 
 		 
-		 
+	btnaddpic.setOnClickListener(new OnClickListener() {
+		
+		
+
+	
+
+		@Override
+		public void onClick(View arg0) {
+		Toast.makeText(getActivity(), "ok", Toast.LENGTH_LONG).show();
+		Intent i = new Intent(
+				Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+	getActivity().startActivityFromFragment(RegisterFragment.this, i,
+			RESULT_LOAD_IMAGE);	
+		}
+	}); 
 		 
 		 
 		 
 		 
 		 return view;
+	}
+	
+	
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RESULT_LOAD_IMAGE
+				&& resultCode == Activity.RESULT_OK && null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+			Cursor cursor = getActivity().getContentResolver().query(
+					selectedImage, filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+
+//			ImageView imageView = (ImageView) dialog
+//					.findViewById(R.id.imageView1);
+//			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+		}
+
 	}
 
 	private EditText findViewById(int edittextuser) {
