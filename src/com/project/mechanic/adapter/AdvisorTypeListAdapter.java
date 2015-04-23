847f7e83a0682524eb8ebd361aad452dfa2299f3
arabian <m.arabian55@yahoo.com>
2015-04-23 19:51:08 +0430

    تغیرات فونت

--- src/com/project/mechanic/adapter/AdvisorTypeListAdapter.java
+++ src/com/project/mechanic/adapter/AdvisorTypeListAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.project.mechanic.MainActivity;
 import com.project.mechanic.R;
 import com.project.mechanic.entity.AdvisorType;
-import com.project.mechanic.entity.City;
-import com.project.mechanic.entity.Executertype;
-import com.project.mechanic.entity.ListItem;
-import com.project.mechanic.entity.Province;
-import com.project.mechanic.fragment.AdvisorTypeFragment;
-import com.project.mechanic.fragment.CityFragment;
 import com.project.mechanic.fragment.ExecutertypeFragment;
 //import com.project.mechanic.fragment.ExecutertypeFragment;
 import com.project.mechanic.fragment.ProvinceFragment;
@@ -52,15 +46,22 @@ public class AdvisorTypeListAdapter extends ArrayAdapter<AdvisorType> {
 		LayoutInflater myInflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		convertView = myInflater.inflate(R.layout.main_item_list, parent, false);
+		convertView = myInflater
+				.inflate(R.layout.main_item_list, parent, false);
 
-		convertView = myInflater.inflate(R.layout.row_advisortype, parent, false);
+		convertView = myInflater.inflate(R.layout.row_advisortype, parent,
+				false);
 
-		TextView tx1 = (TextView) convertView.findViewById(R.id.RowAdvisortypetxt);
+		TextView tx1 = (TextView) convertView
+				.findViewById(R.id.RowAdvisortypetxt);
 
-		AdvisorType  AdvisorType = list.get(position);
+		AdvisorType AdvisorType = list.get(position);
 
 		tx1.setText(AdvisorType.getName());
+
+		Typeface typeFace = Typeface.createFromAsset(context.getAssets(),
+				"fonts/BROYA.TTF");
+		tx1.setTypeface(typeFace);
 
 		convertView.setOnClickListener(new OnClickListener() {
 
@@ -71,39 +72,37 @@ public class AdvisorTypeListAdapter extends ArrayAdapter<AdvisorType> {
 				TextView txtName = (TextView) parentlayout
 						.findViewById(R.id.RowAdvisortypetxt);
 				String item = txtName.getText().toString();
-	
-			//	Toast.makeText(context, item , Toast.LENGTH_LONG).show();
-				
-			adapter.open();
-				ArrayList<AdvisorType> allAdvisorType = adapter.getAdvisorTypes();
+
+				// Toast.makeText(context, item , Toast.LENGTH_LONG).show();
+
+				adapter.open();
+				ArrayList<AdvisorType> allAdvisorType = adapter
+						.getAdvisorTypes();
 				int id = 0;
-			for (AdvisorType AdvisorType1 : allAdvisorType) {
-				if (item.equals(AdvisorType1.getName())) {
+				for (AdvisorType AdvisorType1 : allAdvisorType) {
+					if (item.equals(AdvisorType1.getName())) {
 						// check authentication and authorization
 						id = AdvisorType1.getId();
 					}
 				}
 				adapter.close();
-				
-				
-				
+
 				if (id == 1 || id == 2) {
 					FragmentTransaction trans = ((MainActivity) context)
 							.getSupportFragmentManager().beginTransaction();
 					trans.replace(R.id.content_frame, new ProvinceFragment());
 					trans.addToBackStack(null);
 					trans.commit();
-				
-				
+
 				} else if (id == 3) {
 					FragmentTransaction trans = ((MainActivity) context)
 							.getSupportFragmentManager().beginTransaction();
-					trans.replace(R.id.content_frame, new ExecutertypeFragment());
-				trans.addToBackStack(null);
+					trans.replace(R.id.content_frame,
+							new ExecutertypeFragment());
+					trans.addToBackStack(null);
 					trans.commit();
-				} 
-				
-			
+				}
+
 			}
 		});
 		return convertView;
