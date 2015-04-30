2690f2da48262da192d92533f35f671de8887b4b
Maryam <Maryam@10.253.28.13>
2015-04-30 19:38:57 +0430

    star

--- src/com/project/mechanic/model/DataBaseAdapter.java
+++ src/com/project/mechanic/model/DataBaseAdapter.java
@@ -1585,7 +1585,7 @@ public class DataBaseAdapter {
 		Cursor cursor = mDb
 				.rawQuery(
 
-						"Select O.Id, O.Name, O.Phone, O.Email, O.Fax,O.Description, O.Image1, O.Image2, O.Image3, O.Image4,O.Pdf1,O.Pdf2,O.Pdf3,O.Pdf4,O.Address,O.CellPhone,O.ObjectTypeId,O.ObjectBrandTypeId,O.Facebook,O.Instagram,O.LinkedIn,O.Google,O.Site,O.Twitter From "
+						"Select O.Id, O.Name, O.Phone, O.Email, O.Fax,O.Description, O.Image1, O.Image2, O.Image3, O.Image4,O.Pdf1,O.Pdf2,O.Pdf3,O.Pdf4,O.Address,O.CellPhone,O.ObjectTypeId,O.ObjectBrandTypeId,O.Facebook,O.Instagram,O.LinkedIn,O.Google,O.Site,O.Twitter,O.rate From "
 								+ TableObject
 								+ " as O inner join "
 								+ TableObjectInCity
@@ -1602,7 +1602,7 @@ public class DataBaseAdapter {
 					cursor.getInt(12), cursor.getInt(13), cursor.getString(14),
 					cursor.getString(15), cursor.getString(16),
 					cursor.getString(17), cursor.getString(18),
-					cursor.getString(19), cursor.getInt(25));
+					cursor.getString(19), cursor.getInt(24));
 			result.add(tempObject);
 		}
 		return result;
