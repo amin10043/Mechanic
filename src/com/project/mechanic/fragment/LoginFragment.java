541b382bdf8f2b8d9a27fa8a8b96dcf6f63072dc
amin <amin@AminHosseini-PC>
2015-05-10 04:02:17 -0700

    بهبود لاگین

--- src/com/project/mechanic/fragment/LoginFragment.java
+++ src/com/project/mechanic/fragment/LoginFragment.java
@@ -6,11 +6,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.mechanic.MainActivity;
