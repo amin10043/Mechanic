de9b6bfe49a5ae070a2232f49db672e33b0ffb55
BD <BD@zahra>
2015-06-11 13:35:33 +0100

    repire crop image in editpersonal fragment

--- src/com/project/mechanic/fragment/EditPersonalFragment.java
+++ src/com/project/mechanic/fragment/EditPersonalFragment.java
@@ -1,10 +1,19 @@
 package com.project.mechanic.fragment;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
@@ -26,13 +35,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.mechanic.R;
+import com.project.mechanic.adapter.CropingOptionAdapter;
+import com.project.mechanic.entity.CropingOption;
 import com.project.mechanic.entity.Users;
 import com.project.mechanic.model.DataBaseAdapter;
 import com.project.mechanic.utility.ServiceComm;
 import com.project.mechanic.utility.Utility;
 
 public class EditPersonalFragment extends Fragment {
-
+	private static final int CAMERA_CODE = 101, GALLERY_CODE = 201,
+			CROPING_CODE = 301;
 	protected static final int RESULT_LOAD_IMAGE = 1;
 	DataBaseAdapter dbAdapter;
 	ServiceComm service;
@@ -40,6 +52,8 @@ public class EditPersonalFragment extends Fragment {
 	LinearLayout.LayoutParams lp2;
 	Utility ut;
 	int id;
+	private Uri mImageCaptureUri;
+	private File outPutFile = null;
 
 	public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -51,7 +65,9 @@ public class EditPersonalFragment extends Fragment {
 			Bundle savedInstanceState) {
 		final View view = inflater
 				.inflate(R.layout.fragment_editpersonal, null);
-
+		outPutFile = new File(
+				android.os.Environment.getExternalStorageDirectory(),
+				"temp.jpg");
 		service = new ServiceComm(getActivity());
 
 		ut = new Utility(getActivity());
@@ -190,12 +206,13 @@ public class EditPersonalFragment extends Fragment {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(
-						Intent.ACTION_PICK,
-						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-				getActivity().startActivityFromFragment(
-						EditPersonalFragment.this, i, RESULT_LOAD_IMAGE);
+				// Intent i = new Intent(
+				// Intent.ACTION_PICK,
+				// android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				//
+				// getActivity().startActivityFromFragment(
+				// EditPersonalFragment.this, i, RESULT_LOAD_IMAGE);
+				selectImageOption();
 			}
 		});
 
@@ -203,31 +220,240 @@ public class EditPersonalFragment extends Fragment {
 
 	}
 
+	// public void onActivityResult(int requestCode, int resultCode, Intent
+	// data) {
+	//
+	// super.onActivityResult(requestCode, resultCode, data);
+	//
+	// if (requestCode == RESULT_LOAD_IMAGE
+	// && resultCode == Activity.RESULT_OK && null != data) {
+	// Uri selectedImage = data.getData();
+	// String[] filePathColumn = { MediaStore.Images.Media.DATA };
+	//
+	// Cursor cursor = getActivity().getContentResolver().query(
+	// selectedImage, filePathColumn, null, null, null);
+	// cursor.moveToFirst();
+	//
+	// int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	// String picturePath = cursor.getString(columnIndex);
+	// cursor.close();
+	//
+	// // ImageView btnaddpic1 = (ImageView) view
+	// // .findViewById(R.id.btnaddpic);
+	// img2.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+	// img2.setBackgroundColor(getResources().getColor(
+	// android.R.color.transparent));
+	// img2.setLayoutParams(lp2);
+	// }
+	//
+	// }
+
+	private void selectImageOption() {
+		final CharSequence[] items = { "Capture Photo", "Choose from Gallery",
+				"Cancel" };
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle("Add Photo!");
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int item) {
+
+				if (items[item].equals("Capture Photo")) {
+
+					Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					File f = new File(android.os.Environment
+							.getExternalStorageDirectory(), "temp1.jpg");
+					mImageCaptureUri = Uri.fromFile(f);
+					intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+					startActivityForResult(intent, CAMERA_CODE);
+
+				} else if (items[item].equals("Choose from Gallery")) {
+
+					// Intent i = new Intent(
+					// Intent.ACTION_PICK,
+					// android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+					Intent intent = new Intent();
+					// call android default gallery
+					intent.setType("image/*");
+					intent.setAction(Intent.ACTION_GET_CONTENT);
+					// ******** code for crop image
+					// intent.putExtra("crop", "true");
+					// intent.putExtra("aspectX", 1);
+					// intent.putExtra("aspectY", 1);
+					// intent.putExtra("outputX", 128);
+					// intent.putExtra("outputY", 128);
+
+					// getActivity().startActivityForResult(intent,
+					// GALLERY_CODE);
+
+					try {
+						intent.putExtra("return-data", true);
+						startActivityForResult(Intent.createChooser(intent,
+								"Complete action using"), GALLERY_CODE);
+					} catch (ActivityNotFoundException e) {
+					}
+
+				} else if (items[item].equals("Cancel")) {
+					dialog.dismiss();
+				}
+			}
+		});
+		builder.show();
+	}
+
+	// ////////////////////////////////////////////////////////
+
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == RESULT_LOAD_IMAGE
-				&& resultCode == Activity.RESULT_OK && null != data) {
-			Uri selectedImage = data.getData();
-			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+		if (requestCode == GALLERY_CODE && null != data) {
 
-			Cursor cursor = getActivity().getContentResolver().query(
-					selectedImage, filePathColumn, null, null, null);
-			cursor.moveToFirst();
+			mImageCaptureUri = data.getData();
+			System.out.println("Gallery Image URI : " + mImageCaptureUri);
+			CropingIMG();
 
-			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-			String picturePath = cursor.getString(columnIndex);
-			cursor.close();
+		} else if (requestCode == CAMERA_CODE
+				&& resultCode == Activity.RESULT_OK) {
 
-			// ImageView btnaddpic1 = (ImageView) view
-			// .findViewById(R.id.btnaddpic);
-			img2.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-			img2.setBackgroundColor(getResources().getColor(
-					android.R.color.transparent));
-			img2.setLayoutParams(lp2);
+			System.out.println("Camera Image URI : " + mImageCaptureUri);
+			CropingIMG();
+		} else if (requestCode == CROPING_CODE) {
+
+			try {
+				if (outPutFile.exists()) {
+					Bitmap photo = decodeFile(outPutFile);
+					img2.setImageBitmap(photo);
+				} else {
+					Toast.makeText(getActivity().getApplicationContext(),
+							"Error while save image", Toast.LENGTH_SHORT)
+							.show();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+	}
 
+	private void CropingIMG() {
+
+		final ArrayList cropOptions = new ArrayList();
+
+		final Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setType("image/*");
+
+		ArrayList<ResolveInfo> list = (ArrayList<ResolveInfo>) getActivity()
+				.getPackageManager().queryIntentActivities(intent, 0);
+		int size = list.size();
+		if (size == 0) {
+			// Toast.makeText(this, "Cann't find image croping app",
+			// Toast.LENGTH_SHORT).show();
+			return;
+		} else {
+			intent.setData(mImageCaptureUri);
+			intent.putExtra("outputX", 512);
+			intent.putExtra("outputY", 512);
+			intent.putExtra("aspectX", 1);
+			intent.putExtra("aspectY", 1);
+			intent.putExtra("scale", true);
+
+			// TODO: don't use return-data tag because it's not return large
+			// image data and crash not given any message
+			// intent.putExtra("return-data", true);
+
+			// Create output file here
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outPutFile));
+
+			if (size == 1) {
+				Intent i = new Intent(intent);
+				ResolveInfo res = list.get(0);
+
+				i.setComponent(new ComponentName(res.activityInfo.packageName,
+						res.activityInfo.name));
+
+				startActivityForResult(i, CROPING_CODE);
+			} else {
+				for (ResolveInfo res : list) {
+					final CropingOption co = new CropingOption();
+					PackageManager pm = getActivity().getPackageManager();
+					co.title = pm
+							.getApplicationLabel(res.activityInfo.applicationInfo);
+					co.icon = pm
+							.getApplicationIcon(res.activityInfo.applicationInfo);
+					co.appIntent = new Intent(intent);
+					co.appIntent
+							.setComponent(new ComponentName(
+									res.activityInfo.packageName,
+									res.activityInfo.name));
+					cropOptions.add(co);
+				}
+
+				CropingOptionAdapter adapter = new CropingOptionAdapter(
+						getActivity().getApplicationContext(), cropOptions);
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						getActivity());
+				builder.setTitle("Choose Croping App");
+				builder.setCancelable(false);
+				builder.setAdapter(adapter,
+						new DialogInterface.OnClickListener() {
+
+							public void onClick(DialogInterface dialog,
+									int itemc) {
+
+								final CropingOption co = new CropingOption();
+								co.appIntent = new Intent(intent);
+								startActivityForResult(co.appIntent,
+										CROPING_CODE);
+							}
+						});
+
+				builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+
+						if (mImageCaptureUri != null) {
+							getActivity().getContentResolver().delete(
+									mImageCaptureUri, null, null);
+							mImageCaptureUri = null;
+						}
+					}
+				});
+
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+		}
+	}
+
+	private Bitmap decodeFile(File f) {
+		try {
+			// decode image size
+			BitmapFactory.Options o = new BitmapFactory.Options();
+			o.inJustDecodeBounds = true;
+			BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+
+			// Find the correct scale value. It should be the power of 2.
+			final int REQUIRED_SIZE = 512;
+			int width_tmp = o.outWidth, height_tmp = o.outHeight;
+			int scale = 1;
+			while (true) {
+				if (width_tmp / 2 < REQUIRED_SIZE
+						|| height_tmp / 2 < REQUIRED_SIZE)
+					break;
+				width_tmp /= 2;
+				height_tmp /= 2;
+				scale *= 2;
+			}
+
+			// decode with inSampleSize
+			BitmapFactory.Options o2 = new BitmapFactory.Options();
+			o2.inSampleSize = scale;
+			return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+		} catch (FileNotFoundException e) {
+		}
+		return null;
 	}
 
 }
